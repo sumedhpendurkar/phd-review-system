8c7cefd84ccee1338535b2ca4212bd5f2a9a8c51
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2020-04-02 17:27:10 -0500

    added student information view (incomplete)

--- views/js/app.js
+++ views/js/app.js
@@ -12,10 +12,27 @@ for(y = d.getFullYear(); y > 1850; y--) {
 
         document.getElementById('year').options.add(optn);
 }
+
+function EditReview(){
+  console.log("here");
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var instances = M.FormSelect.init(elems, {});
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {'dismissible': false});
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
 
   // Or with jQuery
 
