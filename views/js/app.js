3dbc7f86f83439d383b313ed4478af3217a3fd5f
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2020-04-10 02:18:27 -0500

    a basic backbone structure of all views; integration of add review view

--- views/js/app.js
+++ views/js/app.js
@@ -1,18 +1,21 @@
 
+function load_monitor(){
+
+  M.textareaAutoResize($('#textarea1'));
+
+}
+console.log("In load Monitor");
 var d = new Date();
 for(y = d.getFullYear(); y > 1850; y--) {
         var optn = document.createElement("OPTION");
         optn.text = y;
         optn.value = y;
 
-        // if year is 2015 selected
         if (y == d.getFullYear()) {
             optn.selected = true;
         }
-
         document.getElementById('year').options.add(optn);
 }
-
 function EditReview(){
   console.log("here");
 }
@@ -24,9 +27,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems, {'dismissible': false});
+  var instances = M.Modal.init(elems, {dismissible: false, startingTop:'1%', endingTop:'0%'});
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
 // Or with jQuery
 
 $(document).ready(function(){
@@ -39,3 +51,11 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('select').formSelect();
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: false
+    });
+});
