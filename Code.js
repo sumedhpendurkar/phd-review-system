1447a5531ffefd5bd5c311baa758a48c97a5c0ff
ratlidav <ratlidav@tamu.edu>
2020-04-17 23:21:18 -0500

    integrated puravs bugfixes to student profile

--- Code.js
+++ Code.js
@@ -1,8 +1,12 @@
-var sheet_url = "https://docs.google.com/spreadsheets/d/1Xf3HsVXoxCE9MDgDdbIj1U8De2NpSEMVTQpN2hlTMGk/edit#gid=0";
+//var student_info_sheet_url = "https://docs.google.com/spreadsheets/d/1Xf3HsVXoxCE9MDgDdbIj1U8De2NpSEMVTQpN2hlTMGk/edit#gid=0";
+
+var student_info_sheet_url = "https://docs.google.com/spreadsheets/d/1vSpjuhHL4BpCgV7-mdMYtCIVd4VfQpKHw16218awcV8/edit#gid=0";
+var faculty_data_sheet_url = "https://docs.google.com/spreadsheets/d/1UWcbToPpGux2qT_u7YHJROfdH_jlSp4-apZGEs52w08/edit#gid=0";
+
 
 function doGet(e){
-  Logger.log(e);
-  Logger.log(ScriptApp.getService().getUrl());
+//  Logger.log(e);
+//  Logger.log(ScriptApp.getService().getUrl());
 //  if (!e.parameter.page){
 //    return HtmlService.createTemplateFromFile("login").evaluate();
 //  }
@@ -11,74 +15,73 @@ function doGet(e){
 //    return HtmlService.createTemplateFromFile(e.parameter['page']).evaluate(); 
 //  }
   
-  return HtmlService.createTemplateFromFile('views/student_info').evaluate(); 
+  return HtmlService.createTemplateFromFile('student_info').evaluate(); 
 }
 
 
 function getProfileInformation() {
+  
   var userInfo = {};
+  
   userInfo.email = Session.getActiveUser().getEmail();
+//  userInfo.firstname = "";
+//  userInfo.lastname = "";
+//  userInfo.UIN = "";
+//  userInfo.startsem = "Fall";
+//  userInfo.qualstatus = "Pass";
+//  userInfo.numattempts = "0";
+//  userInfo.advisor = "Not Selected";
+//  userInfo.coadvisor = "No Co-Advisor";
+//  userInfo.degreeplanstauts = "Yes";
+  
   
 //  var url = "https://docs.google.com/spreadsheets/d/1C5YZ2Lt903A-YGguYQH02JtL9vxs66sMydcD7BeZFJ4/edit#gid=0";
-  var ss = SpreadsheetApp.openByUrl(sheet_url);
+  var ss = SpreadsheetApp.openByUrl(student_info_sheet_url);
   var ws = ss.getSheetByName("data");
   var dataRange = ws.getDataRange();
   var values = dataRange.getValues();
   
   for (var i = 0; i < values.length; i++) {
-    if (values[i][2] == userInfo.email) {
+    if (values[i][3] == userInfo.email) {
       
       userInfo.firstname = values[i][0];
       userInfo.lastname = values[i][1];
-//      userInfo.reviewyear = values[i][3];
-      userInfo.advisor = values[i][4];
+      userInfo.UIN = values[i][2];
+      userInfo.startsem = values[i][4];
+      userInfo.qualstatus = values[i][5];
+      userInfo.numattempts = values[i][6];
+      userInfo.advisor = values[i][7];
+      userInfo.coadvisor = values[i][8];
+      userInfo.degreeplanstatus = values[i][9];
       
-      if(values[i][5]!=""){
-        userInfo.first_sem_date = values[i][5].toISOString().slice(0,10);
+      if(values[i][10]!=""){
+        userInfo.prelime_date = values[i][10].toISOString().slice(0,10);
       }
       
-      if(values[i][6]!=""){
-        userInfo.dplan_filed_date = values[i][6].toISOString().slice(0,10);
+      if(values[i][11]!=""){
+        userInfo.proposal_date = values[i][11].toISOString().slice(0,10);
       }
       
-      if (values[i][7]!=""){
-        userInfo.qual_passed = values[i][7];
+      if(values[i][12]!=""){
+      userInfo.defense_date = values[i][12].toISOString().slice(0,10);
       }
-//      else{
-//        userInfo.qual_approved_sem = "Fall";
-//      }
-      if(values[i][8]!=""){
-        userInfo.prelime_approved_date = values[i][8].toISOString().slice(0,10);
-      }
-      
-      if(values[i][9]!=""){
-        userInfo.proposal_approved_date = values[i][9].toISOString().slice(0,10);
-      }
-      
-//      if(values[i][10]!=""){
-//      userInfo.final_exam_approved_date = values[i][10].toISOString().slice(0,10);
-//      }
-//      
-//      if(values[i][11]!=""){
-//        userInfo.dissertation_approved_date = values[i][11].toISOString().slice(0,10);
-//      }
-      
       break;
     }
   }
-  
   return userInfo;
 }
 
+//////////////////////////////////////////////////////////////////////// Updating Student Informaiton //////////////////////////////////////////////////////
+
 function submitProfile(userInfo){
-  var ss = SpreadsheetApp.openByUrl(sheet_url);
+  var ss = SpreadsheetApp.openByUrl(student_info_sheet_url);
   var ws = ss.getSheetByName("data");
   var dataRange = ws.getDataRange();
   var values = dataRange.getValues();
   var userExists = false;
   
   for (var i = 0; i < values.length; i++) {
-    if (values[i][2] == userInfo.email) {
+    if (values[i][3] == userInfo.email) {
       userExists = true;
       if(userInfo.firstname!=values[i][0]){
         ws.getRange(i+1,0+1).setValue(userInfo.firstname);
@@ -86,48 +89,55 @@ function submitProfile(userInfo){
       if(userInfo.lastname!=values[i][1]){
         ws.getRange(i+1,1+1).setValue(userInfo.lastname);
       }
-//      if(userInfo.reviewyear!=values[i][3]){
-//        ws.getRange(i+1,3+1).setValue(userInfo.reviewyear);
-//      }
-      if(userInfo.advisor!=values[i][4]){
-        ws.getRange(i+1,4+1).setValue(userInfo.advisor);
+      if(userInfo.UIN!=values[i][2]){
+        ws.getRange(i+1,1+1).setValue(userInfo.UIN);
       }
-      if(userInfo.first_sem_date!=values[i][5]){
-        ws.getRange(i+1,5+1).setValue(userInfo.first_sem_date);
+      if(userInfo.startsem!=values[i][4]){
+        ws.getRange(i+1,4+1).setValue(userInfo.startsem);
       }
-      if(userInfo.dplan_filed_date!=values[i][6]){
-        ws.getRange(i+1,6+1).setValue(userInfo.dplan_filed_date);
+      if(userInfo.qualstatus!=values[i][5]){
+        ws.getRange(i+1,5+1).setValue(userInfo.qualstatus);
       }
-      if(userInfo.qual_passed!=values[i][7]){
-        ws.getRange(i+1,7+1).setValue(userInfo.qual_passed);
+      if(userInfo.numattempts!=values[i][6]){
+        ws.getRange(i+1,6+1).setValue(userInfo.numattempts);
       }
-      if(userInfo.prelime_approved_date!=values[i][8]){
-        ws.getRange(i+1,8+1).setValue(userInfo.prelime_approved_date);
+      
+      if(userInfo.advisor!=values[i][7]){
+        ws.getRange(i+1,7+1).setValue(userInfo.advisor);
       }
-      if(userInfo.proposal_approved_date!=values[i][9]){
-        ws.getRange(i+1,9+1).setValue(userInfo.proposal_approved_date);
+      if(userInfo.coadvisor!=values[i][8]){
+        ws.getRange(i+1,8+1).setValue(userInfo.coadvisor);
       }
-//      if(userInfo.final_exam_approved_date!=values[i][10]){
-//        ws.getRange(i+1,10+1).setValue(userInfo.final_exam_approved_date);
-//      }
-//      if(userInfo.dissertation_approved_date!=values[i][11]){
-//        ws.getRange(i+1,11+1).setValue(userInfo.dissertation_approved_date);
-//      }
-      break;
+      
+      if(userInfo.degreeplanstauts!=values[i][9]){
+        ws.getRange(i+1,9+1).setValue(userInfo.degreeplanstauts);
+      }
+      
+      if(userInfo.prelime_date!=values[i][10]){
+        ws.getRange(i+1,10+1).setValue(userInfo.prelime_date);
+      }
+      if(userInfo.proposal_date!=values[i][11]){
+        ws.getRange(i+1,11+1).setValue(userInfo.proposal_date);
+      }
+      if(userInfo.defense_date!=values[i][12]){
+        ws.getRange(i+1,12+1).setValue(userInfo.defense_date);
+      }      
+      break;  
     } 
   }
   
   if (!userExists){ 
-    ws.appendRow([userInfo.firstname,userInfo.lastname,userInfo.email,
-                  /*userInfo.year*/"",userInfo.advisor,userInfo.first_sem_date,
-                  userInfo.dplan_filed_date, userInfo.qual_passed,
-                  userInfo.prelime_approved_date,userInfo.proposal_approved_date
-//                  userInfo.final_exam_approved_date,userInfo.dissertation_approved_date
+    ws.appendRow([userInfo.firstname,userInfo.lastname,userInfo.UIN,userInfo.email,
+                  userInfo.startsem,userInfo.qualstatus,userInfo.numattempts,userInfo.advisor,
+                  userInfo.coadvisor,userInfo.degreeplanstauts,userInfo.prelime_date,userInfo.proposal_date,
+                  userInfo.defense_date
                  ]);
   }
   
 }
 
+
+/////////////////////////////////////////////////////////////////////// File Upload Code///////////////////////////////////////////////////////////////////
 
 function folderExistsIn(parent_folder,folder_name){
   var folders = parent_folder.getFolders();     
@@ -201,19 +211,38 @@ function uploadFileToDrive(content, filename, email,file_type){
 
 function update_file_url(email,file_url){
 //  var url = "https://docs.google.com/spreadsheets/d/1C5YZ2Lt903A-YGguYQH02JtL9vxs66sMydcD7BeZFJ4/edit#gid=0";
-  var ss = SpreadsheetApp.openByUrl(sheet_url);
+  var ss = SpreadsheetApp.openByUrl(student_info_sheet_url);
   var ws = ss.getSheetByName("data");
   var dataRange = ws.getDataRange();
   var values = dataRange.getValues();
   
   for (var i = 0; i < values.length; i++) {
-    if (values[i][2] == email) {
-      ws.getRange(i+1,12+1).setValue(file_url);
+    if (values[i][3] == email) {
+      ws.getRange(i+1,13+1).setValue(file_url);
     }
   }
   
   Logger.log("file url Updated")
 }
+
+//////////////////////////////////////
+
+function get_advisor_list(){
+  
+  var ss = SpreadsheetApp.openByUrl(faculty_data_sheet_url);
+  var ws = ss.getSheetByName("Faculty")
+  var list = ws.getRange(1,1, ws.getRange("A1").getDataRegion().getLastRow(),1).getValues();
+  
+  var advisorlist = list.map(function(r){return '<option value="'+r[0]+'">'+r[0]+'</option>';}).join('');
+  Logger.log(advisorlist)
+  
+  return advisorlist;
+}
+
+
+
+
+////////////////////////////////////////////////////////// Other Stuff ///////////////////////////////////////////////////////////////////////////////
 
 function include(filename){
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
@@ -223,5 +252,6 @@ function include(filename){
 function getScriptUrl(){
   eval(UrlFetchApp.fetch('https://cdn.rawgit.com/medialize/URI.js/gh-pages/src/URI.js').getContentText());
   var uri = URI(ScriptApp.getService().getUrl());
-  return uri.directory('/a/tamu.edu'+uri.directory());
+  var script_url =  uri.directory('/a/tamu.edu'+uri.directory());
+  return script_url;
 }
