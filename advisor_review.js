6b9cc3f8b2365f5b2d1ca5c005a98b51a2ed6df7
ratlidav <ratlidav@tamu.edu>
2020-05-01 14:17:30 -0500

    changed details/review links on faculty view to not be hardcoded

--- advisor_review.js
+++ advisor_review.js
@@ -5,7 +5,7 @@ function getAllStudentRecords() {
   var ss = SpreadsheetApp.openByUrl(url_student_personal_details);
   var ws = ss.getSheetByName("Sheet1");
   
-  var student_records = ws.getRange(2, 1, ws.getRange("A1").getDataRegion().getLastRow() - 1, ws.getRange("A1").getDataRegion().getLastColumn()).getValues();
+  var student_records = ws.getRange(2, 1, ws.getRange("A1").getDataRegion().getLastRow() - 1, ws.getRange("A1").getDataRegion().getLastColumn()-3).getValues(); // note: explicitly excluding date columns (they cause error?)
   Logger.log(student_records);
   return student_records;
 }
